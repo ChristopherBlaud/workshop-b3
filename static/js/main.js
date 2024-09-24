dee9adfa112c91764fe9a9a8e2ade1eb14a94ae3
ChristopherBlaud <contact@cblaud.com>
2024-09-24 07:33:27 +0200

    updated input auth code

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,25 @@
 (() => {
-const passwordInputContainer = document.querySelector(".form__input-container:has(#password)");
+    const passwordInputContainer = document.querySelector('.form__input-container:has(#password)');
 
-const togglePasswordVisibility = (inputContainer) => {
-    const input = inputContainer.querySelector("#password"),
-        eyeIcon = inputContainer.querySelector(".form__input-container__icon-container:has(.eye)"),
-        eyeHiddenIcon = inputContainer.querySelector(".form__input-container__icon-container:has(.eye-hidden)");
+    const togglePasswordVisibility = (inputContainer) => {
+        const input = inputContainer.querySelector('#password'),
+            eyeIcon = inputContainer.querySelector('.form__input-container__icon-container:has(.eye)'),
+            eyeHiddenIcon = inputContainer.querySelector('.form__input-container__icon-container:has(.eye-hidden)');
 
-    eyeIcon.addEventListener("click", () => {
-        eyeIcon.classList.remove("active");
-        eyeHiddenIcon.classList.add("active");
+        eyeIcon.addEventListener('click', () => {
+            eyeIcon.classList.remove('active');
+            eyeHiddenIcon.classList.add('active');
 
-        input.type = "text";
-    });
+            input.type = 'text';
+        });
 
-    eyeHiddenIcon.addEventListener("click", () => {
-        eyeIcon.classList.add("active");
-        eyeHiddenIcon.classList.remove("active");
+        eyeHiddenIcon.addEventListener('click', () => {
+            eyeIcon.classList.add('active');
+            eyeHiddenIcon.classList.remove('active');
 
-        input.type = "password";
-    });
-};
+            input.type = 'password';
+        });
+    };
 
-passwordInputContainer && togglePasswordVisibility(passwordInputContainer);
+    passwordInputContainer && togglePasswordVisibility(passwordInputContainer);
 })();
